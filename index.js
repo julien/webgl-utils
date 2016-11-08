c1d20f2cd464795615f8fb8d39c5d0dd1b9e3748
Julien C <jcastelain@gmail.com>
2016-11-08 23:51:38 +0100

    Add editor config and fix indentation!

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
 
 function createShader(gl, type, src) {
-	const shader = gl.createShader(type);
-	gl.shaderSource(shader, src);
-	gl.compileShader(shader);
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, src);
+  gl.compileShader(shader);
 
-	const res = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-	if (res) {
-		return shader;
-	}
+  const res = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (res) {
+    return shader;
+  }
 
-	const err = gl.getShaderInfoLog(shader);
-	gl.deleteShader(shader);
-	throw new Error(err);
+  const err = gl.getShaderInfoLog(shader);
+  gl.deleteShader(shader);
+  throw new Error(err);
 }
 
 export function createProgram(gl, vsrc, fsrc) {
-	const vshader = createShader(gl, gl.VERTEX_SHADER, vsrc);
-	const fshader = createShader(gl, gl.FRAGMENT_SHADER, fsrc);
-	const program = gl.createProgram();
-	gl.attachShader(program, vshader);
-	gl.attachShader(program, fshader);
-	gl.linkProgram(program);
+  const vshader = createShader(gl, gl.VERTEX_SHADER, vsrc);
+  const fshader = createShader(gl, gl.FRAGMENT_SHADER, fsrc);
+  const program = gl.createProgram();
+  gl.attachShader(program, vshader);
+  gl.attachShader(program, fshader);
+  gl.linkProgram(program);
 
-	const res = gl.getProgramParameter(program, gl.LINK_STATUS);
-	if (res) {
-		return program;
-	}
+  const res = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (res) {
+    return program;
+  }
 
-	const err = gl.getProgramInfoLog(program);
-	gl.deleteProgram(progam);
-	throw new Error(err);
+  const err = gl.getProgramInfoLog(program);
+  gl.deleteProgram(progam);
+  throw new Error(err);
 }
 
 export function getProgramInfo(gl, program) {
-	let info = {};
+  let info = {};
 
-	const attrs = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
-	for (let i = 0; i < attrs; i++) {
-		const attr = gl.getActiveAttrib(program, i);
-		info[attr.name] = gl.getAttribLocation(program, attr.name);
-	}
+  const attrs = gl.getProgramParameter(program, gl.ACTIVE_ATTRIBUTES);
+  for (let i = 0; i < attrs; i++) {
+    const attr = gl.getActiveAttrib(program, i);
+    info[attr.name] = gl.getAttribLocation(program, attr.name);
+  }
 
-	const unis = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
-	for (let i = 0; i < unis; i++) {
-		const uni = gl.getActiveUniform(program, i);
-		info[uni.name] = gl.getUniformLocation(program, uni.name);
-	}
+  const unis = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+  for (let i = 0; i < unis; i++) {
+    const uni = gl.getActiveUniform(program, i);
+    info[uni.name] = gl.getUniformLocation(program, uni.name);
+  }
 
-	return info;
+  return info;
 }
--- test.js
+++ test.js
@@ -8,8 +8,8 @@ attribute vec3 a_position;
 uniform float u_pointSize;
 
 void main() {
-	gl_PointSize = u_pointSize;
-	gl_Position = vec4(a_position, 1.);
+  gl_PointSize = u_pointSize;
+  gl_Position = vec4(a_position, 1.);
 }
 `;
 
@@ -17,24 +17,24 @@ const fsrc = `
 precision mediump float;
 
 void main() {
-	gl_FragColor = vec4(.3, .3, .8, 1.);
+  gl_FragColor = vec4(.3, .3, .8, 1.);
 }
 `;
 
 test('should create a program from shader sources', (t) => {
-	const program = utils.createProgram(gl, vsrc, fsrc);
-	t.ok(program !== null, "program should not be null");
-	t.end();
+  const program = utils.createProgram(gl, vsrc, fsrc);
+  t.ok(program !== null, "program should not be null");
+  t.end();
 });
 
 test('should retrieve attribs and uniforms from a program', (t) => {
-	const program = utils.createProgram(gl, vsrc, fsrc);
-	t.ok(program !== null, "program should not be null");
+  const program = utils.createProgram(gl, vsrc, fsrc);
+  t.ok(program !== null, "program should not be null");
 
-	const programInfo = utils.getProgramInfo(gl, program);
-	t.notOk(programInfo === null, "program info should not be null");
-    t.ok(programInfo.a_position === 0, "a_position attribute should be present and have a value of 0");
-	t.ok(programInfo.u_pointSize !== undefined, "u_pointSize uniform should not be undefined");
-	t.end();
+  const programInfo = utils.getProgramInfo(gl, program);
+  t.notOk(programInfo === null, "program info should not be null");
+  t.ok(programInfo.a_position === 0, "a_position attribute should be present and have a value of 0");
+  t.ok(programInfo.u_pointSize !== undefined, "u_pointSize uniform should not be undefined");
+  t.end();
 });
 
